e4b768f15d62c267cc2285dae219082667f5ee88
konstantinosGkilas <gkilaskonstantinos@gmail.com>
2024-08-24 22:53:24 +0300

    Initialization of mapping-strategy project. 1) Folder structure. 2) Maven dependencies.

---
+++ src/main/java/com/kgkilas/mapping/strategy/MappingStrategy.java
@@ -0,0 +1,12 @@
+package com.kgkilas.mapping.strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingStrategy {
+    public static void main(String[] args) {
+        SpringApplication.run(MappingStrategy.class, args);
+
+    }
+}
