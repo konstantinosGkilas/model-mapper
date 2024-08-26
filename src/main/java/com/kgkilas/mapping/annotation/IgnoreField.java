6a6e23523aa9d57b705841c24308c803a38d5b6d
konstantinosGkilas <gkilaskonstantinos@gmail.com>
2024-08-26 23:30:14 +0300

    Addition of java doc for clarity.

--- src/main/java/com/kgkilas/mapping/annotation/IgnoreField.java
+++ src/main/java/com/kgkilas/mapping/annotation/IgnoreField.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark fields that should be ignored by the GenericMapper.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface IgnoreField {}
+public @interface IgnoreField {
+}
